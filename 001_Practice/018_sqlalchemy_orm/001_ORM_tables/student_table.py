70973bc4d8c24487dbe05db01d22c8c6a8ef1eb2
Rajendra veerelli <84975266+rajendra5811@users.noreply.github.com>
2026-08-17 12:02:25 +0530

    Add files via upload

---
+++ 001_Practice/018_sqlalchemy_orm/001_ORM_tables/student_table.py
@@ -0,0 +1,28 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100))
+
+    orders: Mapped[list["Order"]] = relationship(
+        back_populates="customer"
+    )
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    customer_id: Mapped[int] = mapped_column(ForeignKey("customers.id"))
+
+    customer: Mapped["Customer"] = relationship(
+        back_populates="orders"
+    )
