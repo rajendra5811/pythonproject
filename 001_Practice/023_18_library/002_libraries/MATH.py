16221045a5e2cb61e3c92a8f189dfb09f5799ba8
Rajendra veerelli <84975266+rajendra5811@users.noreply.github.com>
2026-08-26 21:27:50 +0530

    Add files via upload

---
+++ 001_Practice/023_18_library/002_libraries/MATH.py
@@ -0,0 +1,21 @@
+import math
+#Square root of 144
+print(math.sqrt(144))
+a = int(math.sqrt(144))
+print("the square root of 144 is :",a)
+#Ceiling of 4.2
+print(math.ceil(4.2))
+#Floor of 4.8
+print(math.floor(4.8))
+#log of 12 base 2
+print(math.log(12,2))
+print(math.log(2.7343))
+print(math.log2(12))
+#power of x over 3 like x*x*x
+print(math.pow(3,2))
+# factorial
+print(math.factorial(3))
+#fab |-a| = a
+print(math.fabs(-5))
+# value of pi
+print("the value of pi is: ", math.pi)
