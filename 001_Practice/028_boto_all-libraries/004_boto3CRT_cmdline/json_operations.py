b752daef0d0853007926f2f8be04fcfe41b11799
Rajendra veerelli <84975266+rajendra5811@users.noreply.github.com>
2026-09-08 23:30:19 +0530

    Add files via upload

--- 001_Practice/028_boto_all-libraries/004_boto3CRT_cmdline/json_operations.py
+++ 001_Practice/028_boto_all-libraries/004_boto3CRT_cmdline/json_operations.py
@@ -1,24 +1,24 @@
 import json
 import os
 
-def load_json_file(file_path):
+def loadJsonData(file_path):
     blank_json = {}
     if os.path.exists(file_path):
         try:
-            f = open(file_path, 'r')
+            f = open(file_path, "r")
             data = json.load(f)
             f.close()
             return data
-        except ValueError as e:
+        except ValueError: 
             print("Decode error")
             return blank_json
     else:
-        return blank_json
+     return blank_json    
 
 def saveJsonData(file_path, data):
-    try:
-        with open(file_path, 'w') as f:
-            json.dump(data, f, indent=4)
-    except Exception as e:
-        print(f"Error saving JSON data: {e}")
-        
+    blank_json = {}
+    stringified_json_data = json.dumps(data)
+    f = open(file_path, "w")
+    f.write(stringified_json_data)
+    f.close() 
+    return True
