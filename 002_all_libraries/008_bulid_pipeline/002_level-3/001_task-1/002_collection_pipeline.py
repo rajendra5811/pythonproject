99b72447a324aed4829613d954efb8a9a9d80c62
Rajendra veerelli <84975266+rajendra5811@users.noreply.github.com>
2026-09-19 16:15:50 +0530

    Add files via upload

--- 002_all_libraries/008_bulid_pipeline/002_level-3/001_task-1/002_collection_pipeline.py
+++ 002_all_libraries/008_bulid_pipeline/002_level-3/001_task-1/002_collection_pipeline.py
@@ -7,10 +7,10 @@ students = [
     {"name": "John", "city": "DEL"},
     {"name": "Sam", "city": "HYD"},
 ]
+city_count = Counter()
+grouped_students = defaultdict(list)
 for student in students:
     city = student["city"]
-    grouped_students = defaultdict(list)
-    city_count = Counter()
     city_count[city] += 1
     grouped_students[city].append(student["name"])
     print(f"City: {city}, Count: {city_count[city]}, Students: {grouped_students[city]}")
