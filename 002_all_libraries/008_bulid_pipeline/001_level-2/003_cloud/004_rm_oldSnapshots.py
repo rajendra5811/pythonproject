38522700273019fba48f1cfc96459da3570bfe1a
Rajendra veerelli <84975266+rajendra5811@users.noreply.github.com>
2026-09-21 21:35:08 +0530

    Add files via upload

--- 002_all_libraries/008_bulid_pipeline/001_level-2/003_cloud/004_rm_oldSnapshots.py
+++ 002_all_libraries/008_bulid_pipeline/001_level-2/003_cloud/004_rm_oldSnapshots.py
@@ -10,12 +10,10 @@ def remove_old_snapshots():
     thirty_days_ago = datetime.now(timezone.utc) - timedelta(days=30)
 
     old_snapshots = [snap for snap in snapshots if snap['StartTime'] < thirty_days_ago]
-
+    old_snapshots.sort(key=lambda x: x['StartTime'])    
     for snapshot in old_snapshots:
         ec2.delete_snapshot(SnapshotId=snapshot['SnapshotId'])
         print(f"Deleted snapshot: {snapshot['SnapshotId']} created on {snapshot['StartTime']}")
-        ec2.delete_snapshot(SnapshotId=snapshot['SnapshotId'])
-        print(f"Snapshot {snapshot['SnapshotId']} deleted.")
 
 if __name__ == "__main__":
     remove_old_snapshots()
